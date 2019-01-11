feaeec0e3266af3165c1b117591539c4a4796191
shinyscyther <mesibovj@gmail.com>
2019-01-11 11:46:12 -0500

    added the document.ready function to the logic.js file and turned the source of the iDreamBooks reviewinto a link that gets printed to the page

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,36 +1,39 @@
+// Jordan
+// Declare a Book1 variable that will have the data calling the present book. As of this moment, the author data is only there in case we need Jason needs it for a future book if he needs to add it to the query if iDreamBooks doesn't access the right url with just a query of the title
+var Book1 = {
+  title: "Station+Eleven",
+  author: "Emily+St+John+Mandel"
+}
+console.log(Book1);
 
-// <!-- Jordan-->
+$(document).ready(function() {
 
+  function displayBook1Review() {
+    //I changed the queryURL http to https below b/c of an insecure XMLHttpRequest error
+    var queryURL = "https://idreambooks.com/api/books/reviews.json?q=" + Book1.title + "&key=36ecbc8d8618c9f56345cf3e322fa1355b25fc32"
+    console.log(queryURL);
+    console.log(Book1.title);
+    console.log(Book1.author);
+    //make the ajax call to the iDreamBooks API
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log("The response to the ajax call is below");
+      console.log(response);
+      $("#iDB-preview").prepend(`iDreamBooks Rating: ${response.book.rating}%<br><br>`);
+      $("#iDB-preview").append(response.book.critic_reviews[0].snippet);
+      $("#iDB-preview").append(`<br><br>Source: ${response.book.critic_reviews[0].source}`);
+      var reviewLink = response.book.critic_reviews[0].review_link;
+      $("#iDB-preview").append($("<br><a href='" + reviewLink + "'>Click here for the full review</a>"));
+      //Now I just need to figure out how to make the variable reviewLink get printed to the page as a link instead of plain text
+    })
+  }
 
+  displayBook1Review(Book1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
+// Jordan
 
 
 
