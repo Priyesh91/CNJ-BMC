e6a52c1bd977fb84cfc647910882010243556239
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-01-11 11:19:50 -0500

    Added meetup api get to logic.js

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,22 @@
 
+
+// <!-- Jason -->
+  var MeetupAPISignedNextMeetupKey = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Central-Jersey-Sci-Fi-Fantasy-Book-and-Movie-Club&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=3331796&sig=4e946097cff58f9b643141d9dc9b3d33b2bb73ce";
+
+  var corsURL = `https://cors-anywhere.herokuapp.com/${MeetupAPISignedNextMeetupKey}`;
+
+  $.ajax({
+    url: corsURL,
+    method: "GET"
+    }).then(function(response) {
+    console.log(response);
+    var meetings = response;
+    console.log(meetings.results[0].name);
+    }); 
+
+
+// <!-- Jason end -->
+
 // <!-- Jordan-->
 
 
@@ -65,35 +83,6 @@
 
 // <!-- Charity -->
 
-// <!-- Jason -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// <!-- Jason -->
 
 // <!-- priyesh -->
 
