a83a2e21dfb3ee61244942b731c3b3c27694d0a3
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-01-13 00:26:21 -0500

    tried in vain to put a google preview into a bootstrap modal.  No information on google.  idreambooks does it, but their code is inscrutible.  got generic preview popup to work. all in jason-work files.

--- assets/javascript/jason-working.js
+++ assets/javascript/jason-working.js
@@ -1,22 +1,42 @@
 // Jason's temporary javascript workspace for testing before altering the logic.js file
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
 // working on Google Books api
+
+// Loading Google Books api
+  google.books.load();
+
+  function initialize() {
+    var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
+    viewer.load('ISBN:1481440896');
+    viewer.resize();
+  }
+
+  google.books.setOnLoadCallback(initialize);
 
 // variables
 var googleBooks = "";
 
-// GET google book function
-  var googleSearchURL = "";
+// Modal toggle
+$("#googleModalToggler").click(function(){
 
-  $.ajax({
-    url: googleBooksURL,
-    method: "GET"
-    }).then(function(response) {
-    console.log(response);
-    googleBooks = response;
-    
-    }); 
+    $('#googlePreviewModal').modal('toggle');
 
 });
+
+// Bulma modal toggle
+
+// GET google book function
+  // var googleSearchURL = "";
+
+  // $.ajax({
+  //   url: googleBooksURL,
+  //   method: "GET"
+  //   }).then(function(response) {
+  //   console.log(response);
+  //   googleBooks = response;
+    
+  //   }); 
+
+// });
